823f19c09845ab42b683d5e2b9fc699ebe901629
jiro.kirameki <ec2-user@ip-172-31-61-20.ec2.internal>
2018-02-06 07:41:34 +0000

    for heroku

--- Gemfile
+++ Gemfile
@@ -54,3 +54,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 
 gem 'kaminari'
+
+gem :production do
+  gem 'pg', '0.21.0'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131063624) do
+ActiveRecord::Schema.define(version: 20180205032515) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "title"
+    t.integer  "status"
   end
 
 end
